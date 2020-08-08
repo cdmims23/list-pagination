f893f058bf6c9cfca2556dd72e1f8da34ae29c7e
Cori Mims <cdmims21@gmail.com>
2020-08-08 14:00:45 -0400

    Created initial functionality for pagination.

--- js/script.js
+++ js/script.js
@@ -1,39 +1,29 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
+const studentItems = document.querySelectorAll(".student-item");
+const pageDiv = document.querySelector(".page");
 
 
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
 ***/
+
+function showPage(list, page) {
+   let startIndex = (page * 10) - 10;
+   let endIndex = page * 10;
+
+   for(let i = 0; i < list.length; i++) {
+      studentItems[i].style.display = 'none';
+   }
+
+   for(let j = startIndex; j < endIndex; j++) {
+      if (j >= list.length) {
+         break;
+      }
+
+      studentItems[j].style.display = 'block';
+   }
+}
 
 
 
@@ -43,8 +33,22 @@ FSJS project 2 - List Filter and Pagination
    functionality to the pagination buttons.
 ***/
 
+function appendPageLinks(list) {
+   let numberOfLinks = Math.ceil(list.length / 10);
+   let paginationContainer = document.createElement("div");
+   paginationContainer.className += "pagination";
+   pageDiv.appendChild(paginationContainer);
 
+   for(let i = 0; i < numberOfLinks; i++) {
+      let listItem = document.createElement("li");
+      let link = document.createElement("a");
+      link.id = i;
+      link.textContent = `${i + 1}`;
+      listItem.appendChild(link);
+      paginationContainer.appendChild(listItem);
+   }
 
+}
 
+appendPageLinks(studentItems);
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
